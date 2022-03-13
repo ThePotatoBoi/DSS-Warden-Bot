eafd6221ddf1298317680597db5be2695d5a5f61
Astronoot <jorenlangbid2223@gmail.com>
2022-03-13 21:26:44 +0800

    ad

--- events/message.js
+++ events/message.js
@@ -4,9 +4,27 @@ module.exports = {
 	name: 'messageCreate',
 	execute(message, client) {
         var x = [message, client];
-		if (message.content.startWith(PREFIX)) {
+
+		if (message.content.startsWith(PREFIX)) {
+            const messageStr = message.content.substring(4).split(" ");
+            const args = [];
+
+            for (let i = 0; i < messageStr.length; i++) {
+                if (messageStr[i] != "") args[i] = messageStr[i];
+            }
+
             const channel = client.channels.cache.get(message.channelId);
-            channel.send("I received something.");
+            channel.send("I received something: " + args);
+
+            client.commands = new Collection();
+            const commandFiles = fs.readdirSync('./slashCommands').filter(file => file.endsWith('.js'));
+            
+            for (const file of commandFiles) {
+                const command = require(`../slashCommands/${file}`);
+                client.slashCommands.set(command.data.name, command);
+            }
+
         }
+
 	},
 };
--- handlers/loadSlashCommands.js
+++ handlers/loadSlashCommands.js
@@ -2,11 +2,11 @@ function loadCommands(client) {
     const { Collection } = require("discord.js");
     const fs = require("node:fs");
 
-    client.commands = new Collection();
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    client.slashCommands = new Collection();
+    const commandFiles = fs.readdirSync('./slashCommands').filter(file => file.endsWith('.js'));
     
     for (const file of commandFiles) {
-        const command = require(`../commands/${file}`);
+        const command = require(`../slashCommands/${file}`);
         client.slashCommands.set(command.data.name, command);
     }
 }
