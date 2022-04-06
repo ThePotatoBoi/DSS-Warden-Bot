14988118bf1af1c12faaf7d35dbf7e5c55b215ea
Astronoot <jorenlangbid2223@gmail.com>
2022-04-06 22:12:52 +0800

    a

--- events/ready.js
+++ events/ready.js
@@ -3,11 +3,23 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		const textList = ["Lazyass Tyrant", "Hail the Mighty Inspector!", "Dang Panget >:["];
+		const textList = [
+			"Lazyass Tyrant",
+			"Pain and Suffering",
+			"It do be like that",
+			"To live is to suffer",
+			"Vile Degenerate",
+			"Do labor!",
+			"Slavery!",
+			"Uwugaboogaloo",
+			"#NoToPurple",
+			"Dang panget >:[",
+			""
+		];
 		client.user.setPresence({ status: "online" });
 		setInterval(function() {
 			var text = textList[Math.floor(Math.random() * textList.length)];
 			client.user.setActivity(text, { type: "PLAYING"});
-		}, 4000);
+		}, 3000);
 	},
 };
